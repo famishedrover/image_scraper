7404de7d63fd326db133a8356155ea5adfe3085e
famishedrover <mudit.verma2014@gmail.com>
2017-04-08 23:26:40 +0530

    added cli option for save location can handle exceptions

--- main.py
+++ main.py
@@ -6,6 +6,46 @@ from urlparse import urljoin
 import sys 
 from sys import argv
 import time
+import os 
+
+def create_output(dirname  , saveloc = './') :
+	print 'Given save location ',saveloc
+	
+
+	try :
+		dirs = os.listdir(saveloc)
+	except :
+		print 'Supplied Save Location NOT FOUND. ',
+		print 'Save Location set to Current Working Directory'
+		print 'Directory name set to Default - output'
+		dirname = 'output'
+		dirs = os.listdir('./')
+
+	originaldirname = dirname
+	cnt = 0
+	num = str(cnt)
+	while dirname in dirs :
+		# print dirname ,' exists.',
+		dirname = originaldirname + num
+		cnt += 1
+		num = str(cnt)
+
+	try :
+		os.mkdir(saveloc + dirname)
+	except:
+		os.mkdir('./'+ dirname)
+		print 'Save Location set to Current Working Directory'
+	
+	print dirname ,'named Directory created.',
+	print ' Images will be saved here.'
+	#returning outut dir full path 
+	return saveloc + dirname
+
+
+def get_dir() :
+	currdir = os.getcwd()
+	print 'Current Directory :' , currdir
+
 
 
 def make_soup (url) : 
@@ -16,7 +56,7 @@ def make_soup (url) :
 	return BeautifulSoup(html , 'html.parser')
 
 
-def get_images(url) :
+def get_images(url , saveloc = './') :
 	soup = make_soup(url)
 	#found all images html.
 	images = [img for img in soup.findAll('img')]
@@ -25,7 +65,17 @@ def get_images(url) :
 	# 	print i
 
 
-	print ('Downloading into working directory...')
+	get_dir()
+	dirpath = '/'.join(saveloc.split('/')[:-1]) + '/'
+	dirname = saveloc.split('/')[-1]
+	savedir = create_output(dirname , dirpath)
+
+
+
+	print ('Downloading into %s directory...'%savedir)
+
+
+
 	image_links = [eachimg.get('src') for eachimg in images]
 
 	for eachimg in image_links :
@@ -41,7 +91,7 @@ def get_images(url) :
 			time.sleep(1)		#delay
 
 			#saving.
-			with open(filename, 'wb') as of :
+			with open(savedir + '/' + filename, 'wb') as of :
 				shutil.copyfileobj(r.raw,of)
 
 
@@ -60,14 +110,17 @@ def get_images(url) :
 
 if __name__ == '__main__' :
 	try :
-		url = argv[1]
+		url = argv[2]
 	except :
 		url = 'https://en.wikipedia.org/wiki/Image'
-	get_images(url)
+
+	try :
+		dirpath = argv[1]
+	except :
+		currdir = os.getcwd()
+		dirpath = currdir + '/output'
 
 
-
-
-
+	get_images(url , saveloc = dirpath)
 
 
